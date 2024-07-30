c792c8a03879a35331ce363c310a1d97b26782d5
bng16 <biswajitnag161619@gmail.com>
2024-07-30 12:19:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let arr=[4,73,4,6,23,8544,3,5,34,32,4556,4,45,34,6,345,32,6,34,323,34,5534,45,345,6745,67];
+
+const bubbleSort=(arr)=>{
+    for (let i=0; i<arr.length-1;i++){
+        for (let j=0; j<arr.length-i-1;j++){
+            if (arr[j]>arr[j+1]){
+                let temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+}
+bubbleSort(arr);
+console.log(arr);
