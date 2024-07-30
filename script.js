2905488f34fc3e3ddf5b91a209b39fec5a1beed1
bng16 <biswajitnag161619@gmail.com>
2024-07-30 13:15:58 +0530

    2nd done

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 let arr=[4,73,4,6,23,8544,3,5,34,32,4556,4,45,34,6,345,32,6,34,323,34,5534,45,345,6745,67];
 
-const bubbleSort=(arr)=>{
-    for (let i=0; i<arr.length-1;i++){
-        for (let j=0; j<arr.length-i-1;j++){
-            if (arr[j]>arr[j+1]){
-                let temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
+
+
+// const bubbleSort=(arr)=>{
+//     for (let i=0; i<arr.length-1;i++){
+//         for (let j=0; j<arr.length-i-1;j++){
+//             if (arr[j]>arr[j+1]){
+//                 let temp=arr[j];
+//                 arr[j]=arr[j+1];
+//                 arr[j+1]=temp;
+//             }
+//         }
+//     }
+// }
+// bubbleSort(arr);
+// console.log(arr);
+
+
+
+
+
+const selectionSort=(arr)=>{
+    for(var i=0;i<arr.length-1;i++){
+        for(let j=i+1;j<arr.length;j++){
+            if (arr[j]<arr[i]){
+                arr[i]+=arr[j];
+                arr[j]=arr[i]-arr[j];
+                arr[i]=arr[i]-arr[j];
             }
         }
     }
 }
-bubbleSort(arr);
-console.log(arr);
+
+console.log(arr)
+selectionSort(arr)
+console.log(arr)
